b826f52ab7d6dae7a3dd1051594b6876e9057f9d
vimlesh Yadav <117445589+vimlesh9iitd@users.noreply.github.com>
2026-06-07 22:55:46 +0530

    Update config class to use model_config

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Settings(BaseSettings):
     supabase_url: str = ""
     supabase_anon_key: str = ""
 
-    class Config:
-        env_file = ".env"
+    model_config = {"env_file": ".env"}
 
 settings = Settings()
